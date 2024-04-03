b83fa128017047d43df3c1076abffa1975686697
Benedettopana <benedetto.pana@gmail.com>
2024-04-03 15:36:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from "axios";
+
+export default {
+  // components: {},
+  data() {
+    return {
+      axios,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1></h1>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+h1 {
+  font-weight: 800;
+  color: red;
+}
+</style>
